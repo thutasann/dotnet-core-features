edf699aa8837aa1c7950a876d0c5c914ef8c36df
thutasann <thutasann2002@gmail.com>
2024-01-28 14:30:38 +0800

    chore: refactor js files

--- practical-signalR-mvc/wwwroot/js/message.js
+++ practical-signalR-mvc/wwwroot/js/message.js
@@ -2,21 +2,23 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl('/messages').build()
 
-connection.on('ReceiveMessage', function (message) {
-    var msg = message.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-    var div = document.createElement('div')
-    div.innerHTML = msg + '<hr/>'
-    document.getElementById('messages').appendChild(div)
+connection.on('ReceiveMesssage', function (message) {
+  console.log('message', message)
+  var msg = message.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  var div = document.createElement('div')
+  div.innerHTML = msg + '<hr/>'
+  document.getElementById('messages').appendChild(div)
 })
 
 connection.start().catch(function (err) {
-    return console.error('Connection Error', err.toString())
+  return console.error('Connection Error', err.toString())
 })
 
 document.getElementById('sendButton').addEventListener('click', function (event) {
-    event.preventDefault()
-    var message = document.getElementById('messageInput').value
-    connection.invoke('SendMessageToAll', message).catch(function (err) {
-        return console.error('SendMessageToAll Error', err.toString())
-    })
+  console.log('msg')
+  var message = document.getElementById('messageInput').value
+  connection.invoke('SendMessageToAll', message).catch(function (err) {
+    return console.error('SendMessageToAll Error', err.toString())
+  })
+  event.preventDefault()
 })
