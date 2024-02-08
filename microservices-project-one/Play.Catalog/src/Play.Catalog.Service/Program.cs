5f40ca721e48ac564a4834733f1ce6c15f7d3a7e
thutasann <thutasann2002@gmail.com>
2024-02-09 00:47:42 +0800

    feat: BsonSerializer

--- microservices-project-one/Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ microservices-project-one/Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,10 +1,15 @@
 using dotnet_pokemon_review.Middleware;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using Play.Catalog.Service.Interfaces;
 using Play.Catalog.Service.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
