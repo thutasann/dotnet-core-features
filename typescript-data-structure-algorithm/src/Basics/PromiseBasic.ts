603facf3931752b5231811f3aa1854b0dc948328
thutasann <thutasann2002@gmail.com>
2024-02-25 23:44:52 +0800

    add: ts promise all

--- typescript-data-structure-algorithm/src/Basics/PromiseBasic.ts
+++ typescript-data-structure-algorithm/src/Basics/PromiseBasic.ts
@@ -37,4 +37,18 @@ export class PromiseBasic {
             }, 500)
         })
     }
+
+    public PromiseAllSample() {
+        const promise1 = Promise.resolve(3)
+        const promise2 = 42
+        const promise3 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('foo')
+            }, 100)
+        })
+
+        Promise.all([promise1, promise2, promise3]).then((values) => {
+            console.log(values) // 3, 42, 'foo'
+        })
+    }
 }
